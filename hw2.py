44a9476a34b319d9e33c4a26bea3bae7ce76eb3b
Naimul Hoque <naimul@ucsb.edu>
2020-08-26 17:25:48 -0700

    Finished Program.

--- hw2.py
+++ hw2.py
@@ -35,7 +35,6 @@ def dot_kf(u, v):
     """
     # TODO: implement the kernel function
     # Example: Given [x1 x2 x3] and [y1 y2 y3], the dot product will be x1y1 + x2y2 + x3y3. Lists must be same length
-
     dotProductValue = 0 # This will hold the dot product result
     if(len(u) != len(v)): # If both lists are not of same length, can not compute dot product. Output error message
         print("Error: Lengths of List don't match")
@@ -43,7 +42,6 @@ def dot_kf(u, v):
     else:
         for i in range(len(u)): # For the length of any list. In this case, u
             dotProductValue += float(u[i])*float(v[i]) # Take the sum of the products of u and v element wise
-
     return dotProductValue
 
 def poly_kernel(d):
@@ -80,14 +78,11 @@ def exp_kernel(s):
         subtractedMagnitude = 0 # Represents the magnitude of the subtracted list
         # Obtains the subtracted list
         for i in range(len(u)):
-            subtractedList.append(u[i] - v[i])
-
+            value = float(u[i]) - float(v[i])
+            subtractedList.append(value)
         # Obtains the magnitude
         for i in range(len(subtractedList)):
             subtractedMagnitude += subtractedList[i] ** 2
-        
-        
-
         return math.exp((-1*subtractedMagnitude)/(2*(s**2)))
     return kf
 
@@ -105,7 +100,7 @@ class Perceptron:
 
     def train(self, data):
         # TODO: Main function - train the perceptron with data
-        myLabels = []
+        myLabels = [] # This will hold the data labels in numerical values
         # For loop converts labels to numerical values
         for i in range(len(data.labels)):
             if(data.labels[i] == "Iris-setosa"):
@@ -117,11 +112,12 @@ class Perceptron:
         while(converged == False):
             converged = True # If converged remains true, loop will exit
             for i in range(len(data.features)):
-                trainPrediction = self.calculatePrediction(data.features[i])
-                if(self.update(trainPrediction, myLabels[i]) == True):
+                trainPrediction = self.calculatePrediction(data.features[i]) # Calculate the prediction
+                if(self.update(trainPrediction, myLabels[i]) == True): # If update is necessary, we need to add missedLabl and missedPoints to respective list. converged is set to false
                     self.MissedLabels.append(myLabels[i])
                     self.MissedPoints.append(data.features[i])
-        return self.MissedLabels, self.MissedPoints
+                    converged = False
+        return self.MissedLabels, self.MissedPoints #
 
     def update(self, point, label):
         """
@@ -140,7 +136,6 @@ class Perceptron:
             is_mistake = True
         else:
             is_mistake = False
-        
         return is_mistake
 
     def predict(self, point):
@@ -148,51 +143,71 @@ class Perceptron:
         Given a point, predicts the label of that point (1 or -1).
         """
         # TODO
-        return
+        summation = 0
+        # Use formula from Lecture Slide 9
+        for i in range(len(self.MissedLabels)):
+            summation += self.MissedLabels[i] * self.kf(point,self.MissedPoints[i])
+        return np.sign(summation) # Return -1 or 1
 
     def test(self, data):
         predictions = []
         # TODO: given data and a perceptron - return a list of integers (+1 or -1).
         # +1 means it is Iris Setosa
         # -1 means it is not Iris Setosa
-
         predictions = [] # Create an empty list.
-        for i in range(len(data)): # Iterate through data and obtain prediction. Store in list and return once process is finished
-            predictions.append(predict(data))
+        for i in range(len(data.features)): # Iterate through data and obtain prediction. Store in list and return once process is finished
+            predictions.append(self.predict(data.features[i]))
         return predictions
 
+    # Function is used to calculate the summation portion
     def calculatePrediction(self,point):
-        if not self.MissedPoints:
+        if not self.MissedPoints: #Checks if it is an empty list
             trainPrediction = 0
         else:
             trainPrediction = 0
+            # If not empty, calculate summation
             for i in range(len(self.MissedLabels)):
                 trainPrediction += self.MissedLabels[i] * self.kf(point,self.MissedPoints[i])
-        
+        # Use Signum function to convert to -1, 0 , or 1
         trainPrediction = np.sign(trainPrediction)
         return trainPrediction
 
 
 # Feel free to add any helper functions as needed.
-#if __name__ == '__main__':
-def accuracyTest(testingLabels, predictionValues):
-    correct = 0 # Holds total correct
-    for i in range(len(testingLabels)): # Iterate through labels and predictionValueList
-        if((testingLabels[i] == "Iris-setosa") and (predictionValues[i] == 1)):
-            correct += 1 # If the label was Iris-setosa and prediction was 1, it is correct
-        elif(((testingLabels[i] == "Iris-virginia") or ((testingLabels[i] == "Iris-versicolor"))) and (predictionValues[i] == -1)):
-            correct += 1 # If label is any other species and prediction is -1, it is also correct
-    return str((correct/len(testingLabels)) * 100) + "%"
+if __name__ == '__main__':
+# Gets the accuracy percentage
+    def accuracyTest(testingLabels, predictionValues):
+        correct = 0 # Holds total correct
+        for i in range(len(testingLabels)): # Iterate through labels and predictionValueList
+            if(testingLabels[i] == predictionValues[i]):
+                correct += 1
+        return str((correct/len(testingLabels)) * 100) + "%"
 
 # Obtain training data.
 trainData = Data()
 trainData = read_data("hw2_train.txt")
-# Convert data to homogenous coordinates
+# Convert data to homogenous data
 for i in trainData.features:
     i.append(1)
 # Send data to train
-myPerceptron = Perceptron(dot_kf, 0.2)
-myMissedLabels, myMissedPoints = myPerceptron.train(trainData)
-print(myMissedLabels, myMissedPoints)
+myPerceptron = Perceptron(poly_kernel(2), 0.2) # change kf to apply different kernel
+myPerceptron.train(trainData)
+# Retrieve Test Data
+testData = Data()
+testData = read_data("hw2_test.txt")
+# Convert data to homogenous data
+for i in testData.features:
+    i.append(1)
+# Obtain Prediction
+predictions = myPerceptron.test(testData)
+# Convert test labels to numerical value
+myTestLabels = []
+for i in testData.labels: 
+    if(i == "Iris-setosa" ):
+        myTestLabels.append(1)
+    else:
+        myTestLabels.append(-1)
+# Print accuracy
+print(accuracyTest(myTestLabels, predictions))
 
 
